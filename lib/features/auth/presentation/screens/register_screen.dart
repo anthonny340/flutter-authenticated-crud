e1db2bfedbf6246f6a7309d5a233220452eea591
anthonny340 <tonysolorzano880@gmail.com>
2025-10-27 23:29:23 -0500

    chore(register_screen): modificado y terminado

--- lib/features/auth/presentation/screens/register_screen.dart
+++ lib/features/auth/presentation/screens/register_screen.dart
@@ -1,140 +1,175 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:teslo_shop/features/auth/presentation/providers/providers.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
-
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    final size = MediaQuery.of(context).size;
+    // final size = MediaQuery.of(context).size;
     final scaffoldBackgroundColor = Theme.of(context).scaffoldBackgroundColor;
     final textStyles = Theme.of(context).textTheme;
 
-    return GestureDetector(
-      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-      child: Scaffold(
-        body: GeometricalBackground( 
-          child: SingleChildScrollView(
-            physics: const ClampingScrollPhysics(),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox( height: 80 ),
-                // Icon Banner
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      onPressed: (){
-                        if ( !context.canPop() ) return;
+    return SafeArea(
+      child: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: Scaffold(
+            body: GeometricalBackground(
+                child: SingleChildScrollView(
+          physics: const ClampingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 60),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        if (!context.canPop()) return;
                         context.pop();
-                      }, 
-                      icon: const Icon( Icons.arrow_back_rounded, size: 40, color: Colors.white )
-                    ),
-                    const Spacer(flex: 1),
-                    Text('Crear cuenta', style: textStyles.titleLarge?.copyWith(color: Colors.white )),
-                    const Spacer(flex: 2),
-                  ],
-                ),
-
-                const SizedBox( height: 50 ),
-    
-                Container(
-                  height: size.height - 260, // 80 los dos sizebox y 100 el ícono
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: scaffoldBackgroundColor,
-                    borderRadius: const BorderRadius.only(topLeft: Radius.circular(100)),
+                      },
+                      icon: const Icon(Icons.arrow_back_rounded,
+                          size: 40, color: Colors.white)),
+                  const SizedBox(width: 10),
+                  Text(
+                    'Crear cuenta',
+                    style: textStyles.titleLarge?.copyWith(color: Colors.white),
                   ),
-                  child: const _RegisterForm(),
-                )
-              ],
-            ),
-          )
-        )
+                  const SizedBox(height: 40),
+                ],
+              ),
+              const SizedBox(height: 60),
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  return Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: scaffoldBackgroundColor,
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(100)),
+                    ),
+                    child: const _RegisterForm(),
+                  );
+                },
+              )
+            ],
+          ),
+        ))),
       ),
     );
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends ConsumerWidget {
   const _RegisterForm();
 
+  void showSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final registerForm = ref.watch(registerFormProvider);
+    ref.listen(
+      authProvider,
+      (previous, next) {
+        if (next.errorMessage.isEmpty) return;
+        showSnackBar(context, next.errorMessage);
 
+        ref.read(authProvider.notifier).clearErrorMessage();
+      },
+    );
     final textStyles = Theme.of(context).textTheme;
+    String password = '';
+    String confirmPassword = '';
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 50),
-      child: Column(
-        children: [
-          const SizedBox( height: 50 ),
-          Text('Nueva cuenta', style: textStyles.titleMedium ),
-          const SizedBox( height: 50 ),
-
-          const CustomTextFormField(
-            label: 'Nombre completo',
-            keyboardType: TextInputType.emailAddress,
-          ),
-          const SizedBox( height: 30 ),
-
-          const CustomTextFormField(
-            label: 'Correo',
-            keyboardType: TextInputType.emailAddress,
-          ),
-          const SizedBox( height: 30 ),
-
-          const CustomTextFormField(
-            label: 'Contraseña',
-            obscureText: true,
-          ),
-    
-          const SizedBox( height: 30 ),
-
-          const CustomTextFormField(
-            label: 'Repita la contraseña',
-            obscureText: true,
-          ),
-    
-          const SizedBox( height: 30 ),
-
-          SizedBox(
-            width: double.infinity,
-            height: 60,
-            child: CustomFilledButton(
-              text: 'Crear',
-              buttonColor: Colors.black,
-              onPressed: (){
-
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 50),
+        child: Column(
+          children: [
+            const SizedBox(height: 45),
+            Text('Nueva cuenta', style: textStyles.titleMedium),
+            const SizedBox(height: 45),
+            CustomTextFormField(
+              label: 'Nombre completo',
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (value) {
+                ref.read(registerFormProvider.notifier).onEmailChange;
               },
-            )
-          ),
-
-          const Spacer( flex: 2 ),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('¿Ya tienes cuenta?'),
-              TextButton(
-                onPressed: (){
-                  if ( context.canPop()){
-                    return context.pop();
-                  }
-                  context.go('/login');
-                  
-                }, 
-                child: const Text('Ingresa aquí')
-              )
-            ],
-          ),
-
-          const Spacer( flex: 1),
-        ],
+              errorMessage: registerForm.isFormPosted
+                  ? registerForm.fullName.errorMessage
+                  : null,
+            ),
+            const SizedBox(height: 30),
+            CustomTextFormField(
+              label: 'Correo',
+              keyboardType: TextInputType.emailAddress,
+              onChanged: ref.read(registerFormProvider.notifier).onEmailChange,
+              errorMessage: registerForm.isFormPosted
+                  ? registerForm.email.errorMessage
+                  : null,
+            ),
+            const SizedBox(height: 30),
+            CustomTextFormField(
+              label: 'Contraseña',
+              obscureText: true,
+              onChanged: (value) {
+                password = value;
+                ref
+                    .read(registerFormProvider.notifier)
+                    .onPasswordChanged(value);
+              },
+              errorMessage: registerForm.isFormPosted
+                  ? registerForm.password.errorMessage
+                  : null,
+            ),
+            const SizedBox(height: 30),
+            CustomTextFormField(
+              label: 'Repita la contraseña',
+              obscureText: true,
+              onChanged: (value) {
+                confirmPassword = value;
+              },
+              errorMessage: registerForm.isFormPosted
+                  ? registerForm.password.errorMessage
+                  : null,
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: CustomFilledButton(
+                  text: 'Crear',
+                  buttonColor: Colors.black,
+                  onPressed: () {
+                    ref.read(registerFormProvider.notifier).onFormSubmit();
+                  },
+                )),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('¿Ya tienes cuenta?'),
+                TextButton(
+                    onPressed: () {
+                      if (context.canPop()) {
+                        return context.pop();
+                      }
+                      context.go('/login');
+                    },
+                    child: const Text('Ingresa aquí'))
+              ],
+            ),
+            const SizedBox(height: 40)
+          ],
+        ),
       ),
     );
   }
