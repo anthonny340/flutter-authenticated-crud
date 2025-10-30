7a72198e2958b5fc269de56ad682fa2cce4fa9a6
anthonny340 <tonysolorzano880@gmail.com>
2025-10-30 17:13:45 -0500

    feat: se implemento keyValueStorageService para guardar el token

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,18 +1,27 @@
 import 'package:flutter_riverpod/legacy.dart';
 import 'package:teslo_shop/features/auth/domain/domain.dart';
 import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service_impl.dart';
 
 //! Tercero crear el provider
 final authProvider =
     StateNotifierProvider.autoDispose<AuthNotifier, AuthState>((ref) {
   final authRepositoryImpl = AuthRepositoryImpl();
-  return AuthNotifier(authRepository: authRepositoryImpl);
+  final keyValueStorageServiceImpl = KeyValueStorageServieImpl();
+  return AuthNotifier(
+      authRepository: authRepositoryImpl,
+      keyValueStorageService: keyValueStorageServiceImpl);
 });
 
 //! Segundo se crear el Notifier
 class AuthNotifier extends StateNotifier<AuthState> {
   final AuthRepository authRepository;
-  AuthNotifier({required this.authRepository}) : super(AuthState());
+  final KeyValueStorageService keyValueStorageService;
+  AuthNotifier({
+    required this.authRepository,
+    required this.keyValueStorageService,
+  }) : super(AuthState());
 
   // Estos metodos terminan delegando el trabajo al repositorio por este motivo se crea
   // una instancia de AuthRepository en el provider y se lo manda como parametro al Notifier
@@ -46,8 +55,9 @@ class AuthNotifier extends StateNotifier<AuthState> {
 
   void checkAuthStatus() async {}
 
-  void _setLoggedUser(User user) {
-    //TODO: necesito guardar el token fisicamente para que sea persistente
+  void _setLoggedUser(User user) async {
+    await keyValueStorageService.setKeyValue('toker', user.token);
+
     state = state.copyWith(
       user: user,
       authStatus: AuthStatus.autenticated,
@@ -56,7 +66,8 @@ class AuthNotifier extends StateNotifier<AuthState> {
   }
 
   Future<void> logout([String? errorMessage]) async {
-    //TODO: limpia token
+    await keyValueStorageService.removeKey('token');
+
     state = state.copyWith(
         authStatus: AuthStatus.notAutenticated,
         user: null,
