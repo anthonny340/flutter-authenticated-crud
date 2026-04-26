b55b56888239c67533febbd3e82de229a9c184e6
Anthonny Sacheri <tonysolorzano880@gmail.com>
2026-04-26 23:25:20 +0200

    feat: implementacion de productsProvider en products_screen
    - Se instalo la dependencia flutter_staggered_grid_view para usar despues
    cuando se muestren los productos en la pantalla principal

--- lib/features/products/presentation/screens/products_screen.dart
+++ lib/features/products/presentation/screens/products_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/products/presentation/providers/providers.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
 class ProductsScreen extends StatelessWidget {
@@ -6,36 +8,45 @@ class ProductsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final scaffoldKey = GlobalKey<ScaffoldState>();
 
     return Scaffold(
-      drawer: SideMenu( scaffoldKey: scaffoldKey ),
+      drawer: SideMenu(scaffoldKey: scaffoldKey),
       appBar: AppBar(
         title: const Text('Products'),
         actions: [
-          IconButton(
-            onPressed: (){}, 
-            icon: const Icon( Icons.search_rounded)
-          )
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search_rounded))
         ],
       ),
       body: const _ProductsView(),
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Nuevo producto'),
-        icon: const Icon( Icons.add ),
+        icon: const Icon(Icons.add),
         onPressed: () {},
       ),
     );
   }
 }
 
-
-class _ProductsView extends StatelessWidget {
+class _ProductsView extends ConsumerStatefulWidget {
   const _ProductsView();
 
   @override
+  _ProductsViewState createState() => _ProductsViewState();
+}
+
+class _ProductsViewState extends ConsumerState<_ProductsView> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    ref.watch(productsProvider);
     return const Center(child: Text('Eres genial!'));
   }
 }
