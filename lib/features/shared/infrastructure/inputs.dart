ce75659eec9792133ab087153afb5e981d4719de
Anthonny Sacheri <tonysolorzano880@gmail.com>
2026-05-03 11:12:23 +0200

    feat: creacion de inputs faltantes para validar formulario de product_screen

--- lib/features/shared/infrastructure/inputs.dart
+++
@@ -1,4 +0,0 @@
-export 'inputs/email.dart';
-export 'inputs/password.dart';
-export 'inputs/confirm_password.dart';
-export 'inputs/full_name.dart';
---
+++ lib/features/shared/infrastructure/inputs/inputs.dart
@@ -0,0 +1,8 @@
+export 'confirm_password.dart';
+export 'email.dart';
+export 'full_name.dart';
+export 'password.dart';
+export 'price.dart';
+export 'slug.dart';
+export 'stock.dart';
+export 'title.dart';
